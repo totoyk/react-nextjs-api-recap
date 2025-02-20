1dc413d3604d3f9f12449a0a80313a77a33e7fd0
me <you@example.com>
2025-02-20 16:53:31 +0900

    update added apikey check

--- nextjs-api-recap/src/pages/api/config.js
+++ nextjs-api-recap/src/pages/api/config.js
@@ -2,6 +2,13 @@ import fs from 'fs'
 import path from 'path'
 
 export default function handler(req, res) {
+  const apiKey = req.headers['x-api-key']
+
+  const expectedApiKey = process.env.API_KEY
+  if (apiKey !== expectedApiKey) {
+    return res.status(403).json({ message: 'Forbidden: Invalid API key' })
+  }
+
   const filePath = path.join(process.cwd(), 'config/read.json')
 
   try {
